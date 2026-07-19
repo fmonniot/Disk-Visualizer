e4b85fc12bd051a3801cc61580923d32b3f158cb
François Monniot <francoismonniot@gmail.com>
2026-07-19 16:38:06 -0700

    Initial Commit

---
+++ Disk VisualizerTests/Disk_VisualizerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Disk_VisualizerTests.swift
+//  Disk VisualizerTests
+//
+//  Created by François Monniot on 7/19/26.
+//
+
+import Testing
+@testable import Disk_Visualizer
+
+struct Disk_VisualizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
